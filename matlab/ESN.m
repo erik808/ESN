e6b703ee07146ff42300d969116d1211b562160a
Erik <t.e.mulder@uu.nl>
2020-06-10 19:54:00 +0200

    Implemented a bias and restructured the scaling added the option to append or replace state components with squares added the possibility to mean center X and trainY factored usage of the network in apply() factored computation of scaling in computeScaling()

--- matlab/ESN.m
+++ matlab/ESN.m
@@ -29,25 +29,39 @@ classdef ESN < handle
         % leaking rate
         alpha (1,1) double {mustBeNonnegative} = 1.0;
 
+        % bias
+        bias (1,:) double {mustBeNumeric} = 0.0; % bias vector
+
         shiftU (1,:) double {mustBeNumeric} % input shift
         shiftY (1,:) double {mustBeNumeric} % output shift
 
         scaleU (1,:) double {mustBeNumeric} % input scaling
         scaleY (1,:) double {mustBeNumeric} % output scaling
 
-        % 'zeros': no shift,
-        % 'meanCentering': center each column around its mean
-        defaultShift (1,1) string = 'zeros';
-
-        % 'maxAbs': using the absolute maximum of the full training data
-        % 'maxAbsCol': using a columnwise absolute maximum
-        defaultScaling (1,1) string = 'maxAbs';
+        % 'none'        : no scaling or user defined
+        % 'minMax1'     : rescale range of features to [0,1]
+        % 'minMax2'     : rescale range of features to [-1,1]
+        % 'standardize' : rescale feature range to zero mean and unit variance
+        scalingType (1,1) string = 'none';
 
         X (:,:) double {mustBeNumeric} % reservoir state activations
+
+        % for fitting take the square of the even components, this fixes
+        % issues with symmetry
+        % 'disabled'
+        % 'append'
+        % 'even'
+        squaredStates (1,1) string = 'disabled';
 
         % control feedthrough of u to y: the input state is appended to the
         % reservoir state activations X and used to fit W_out
         feedThrough (1,1) {mustBeNumericOrLogical} = true;
+
+        % mean center reservoir states before fitting
+        centerX (1,1) {mustBeNumericOrLogical} = false;
+
+        % mean center training output data before fitting
+        centerY (1,1) {mustBeNumericOrLogical} = false;
 
         % set the activation function f: 'tanh'
         activation (1,1) string = 'tanh';
@@ -84,13 +98,14 @@ classdef ESN < handle
         regressionSolver (1,1) string = 'pinv';
 
         % lambda (when using Tikhonov regularization)
-        lambda (1,1) double {mustBeNonnegative} = 1.0e-3;
+        lambda (1,1) double {mustBeNonnegative} = 1.0e-7;
 
         % tolerance for the pseudo inverse
         pinvTol (1,1) double {mustBeNonnegative} = 1.0e-4;
     end
 
     methods
+        %-------------------------------------------------------
         function self = ESN(Nr, Nu, Ny)
         % Constructor
 
@@ -100,13 +115,13 @@ classdef ESN < handle
 
         end
 
+        %-------------------------------------------------------
         function initialize(self)
         % Create W, W_in, W_ofb and set output activation function
 
             self.createW;
             self.createW_in;
             self.createW_ofb;
-
 
             if self.activation == 'tanh'
                 self.f = @(x) tanh(x);
@@ -124,9 +139,16 @@ classdef ESN < handle
                 throw(ME);
             end
 
+            % default scaling (none)
+            self.shiftU = 0;
+            self.shiftY = 0;
+            self.scaleU = 1;
+            self.scaleY = 1;
+
             fprintf('ESN initialization done\n');
         end
 
+        %-------------------------------------------------------
         function createW(self)
         % Create sparse weight matrix with spectral radius rhoMax
 
@@ -167,6 +189,7 @@ classdef ESN < handle
             self.W = self.W * self.rhoMax / mrho;
         end
 
+        %-------------------------------------------------------
         function createW_in(self)
         % Create input weight matrix, sparse or full based on inputMatrixType.
 
@@ -187,6 +210,7 @@ classdef ESN < handle
             self.W_in = self.inAmplitude * self.W_in;
         end
 
+        %-------------------------------------------------------
         function createW_ofb(self)
         % Create output feedback weight matrix
 
@@ -208,6 +232,7 @@ classdef ESN < handle
 
         end
 
+        %-------------------------------------------------------
         function train(self, trainU, trainY)
         % Train the ESN with training data trainU and trainY
 
@@ -222,52 +247,12 @@ classdef ESN < handle
             assert(T == size(trainY,1), 'ESN:dimensionError', ...
                    'input and output training data have different number of samples');
 
+            % Now that we have data we can setup the scaling
+            self.computeScaling(trainU, trainY)
 
-            if (~isempty(self.shiftU)) && (~isempty(self.shiftY))
-                fprintf('ESN shift: user specified\n');
-
-            elseif self.defaultShift == 'zeros'
-                self.shiftU = zeros(1,self.Nu);
-                self.shiftY = zeros(1,self.Ny);
-                fprintf('ESN shift: zeros\n');
-
-            elseif self.defaultShift == 'meanCentering'
-                self.shiftU = mean(trainU);
-                self.shiftY = mean(trainY);
-                fprintf('ESN shift: mean centering\n');
-
-            else
-                ME = MException('ESN:invalidParameter', ...
-                                'invalid defaultShift parameter');
-                throw(ME);
-            end
-
-            % shift training data
-            trainU = trainU - self.shiftU; % TODO factorize with method
-            trainY = trainY - self.shiftY; % - -
-
-            if (~isempty(self.scaleU)) && (~isempty(self.scaleY))
-                fprintf('ESN scaling: user specified\n');
-
-            elseif self.defaultScaling == 'maxAbs'
-                fprintf('ESN scaling: maxAbs\n');
-                self.scaleU = 1.0 ./ max(abs(trainU(:)));
-                self.scaleY = 1.0 ./ max(abs(trainY(:)));
-
-            elseif self.defaultScaling == 'maxAbsCol'
-                fprintf('ESN scaling: maxAbsCol\n');
-                self.scaleU = 1.0 ./ max(abs(trainU));
-                self.scaleY = 1.0 ./ max(abs(trainY));
-
-            else
-                ME = MException('ESN:invalidParameter', ...
-                                'invalid defaultScaling parameter');
-                throw(ME);
-            end
-
-            % scale training data
-            trainU = trainU .* self.scaleU; % - -
-            trainY = trainY .* self.scaleY; % - -
+            % Apply scaling
+            trainU = self.scaleInput(trainU);
+            trainY = self.scaleOutput(trainY);
 
             % initialize activations X
             if self.reservoirStateInit == 'random'
@@ -295,10 +280,23 @@ classdef ESN < handle
             time = tic;
             fprintf('ESN fitting W_out...\n')
 
+            extX = self.X;
+            if self.squaredStates == 'append';
+                extX = [self.X, self.X.^2];
+            elseif self.squaredStates == 'even';
+                extX(:,2:2:end) = extX(:,2:2:end).^2;
+            end
+
             if self.feedThrough
-                extX = [self.X, trainU];
-            else
-                extX = self.X;
+                extX = [extX, trainU];
+            end
+
+            if self.centerX
+                extX = extX - mean(extX);
+            end
+
+            if self.centerY
+                trainY = trainY - mean(trainY);
             end
 
             if self.regressionSolver == 'pinv'
@@ -309,10 +307,12 @@ classdef ESN < handle
 
             elseif self.regressionSolver == 'Tikhonov'
 
-                fprintf('ESN  using Tikhonov regularization, lambda = %1.1e\n', self.lambda)
-                Xnormal = extX'*extX + self.lambda * speye(size(extX,2));
-                b       = extX'*self.if_out(trainY);
-                self.W_out   = (Xnormal \ b)';
+                fprintf('ESN  using Tikhonov regularization, lambda = %1.1e\n', ...
+                        self.lambda)
+                fprintf('     problem size %d x %d\n', size(extX,2), size(extX,2));
+                Xnormal    = extX'*extX + self.lambda * speye(size(extX,2));
+                b          = extX'*self.if_out(trainY);
+                self.W_out = (Xnormal \ b)';
 
             else
                 ME = MException('ESN:invalidParameter', ...
@@ -327,16 +327,86 @@ classdef ESN < handle
             fprintf('ESN training error: %e\n', sqrt(mean((predY(:) - trainY(:)).^2)));
         end
 
+        %-------------------------------------------------------
         function [act] = update(self, state, u, y)
-            pre = self.W*state' + self.W_in*u' + self.W_ofb*y';
+        % update the reservoir state
+            if nargin < 4
+                y = zeros(1, self.Ny);
+            end
+
+            pre = self.W*state' + self.W_in*u' + self.W_ofb*y' + self.bias;
             act = self.alpha * self.f(pre) + (1-self.alpha) * state' + ...
                   self.noiseAmplitude * (rand(self.Nr,1) - 0.5);
         end
 
+        %-------------------------------------------------------
+        function [out] = apply(self, state, u)
+
+            x = state;
+            
+            if self.squaredStates == 'append'
+                x = [state, state.^2];
+            elseif self.squaredStates == 'even'
+                x(2:2:end) = state(2:2:end).^2;
+            end
+
+            if self.feedThrough
+                out = self.f_out(self.W_out * [x'; u'])';
+            else
+                out = self.f_out(self.W_out * x')';
+            end
+        end
+
+        %-------------------------------------------------------
+        function [] = computeScaling(self, U, Y)
+
+            if self.scalingType == 'none'
+                fprintf('ESN scaling: none or user specified\n');
+
+            elseif self.scalingType == 'minMax1'
+                self.scaleU = 1.0 ./ (max(U) - min(U));
+                self.scaleY = 1.0 ./ (max(Y) - min(Y));
+                self.shiftU = min(U);
+                self.shiftY = min(Y);
+                fprintf('ESN scaling: minMax1 [0,1]\n');
+
+            elseif self.scalingType == 'minMax2'
+                self.scaleU = 2.0 ./ (max(U) - min(U));
+                self.scaleY = 2.0 ./ (max(Y) - min(Y));
+                self.shiftU = min(U) + 1 ./ self.scaleU;
+                self.shiftY = min(Y) + 1 ./ self.scaleY;
+                fprintf('ESN scaling: minMax2 [-1,1]\n');
+
+            elseif self.scalingType == 'standardize'
+                self.scaleU = 1.0 ./ std(U);
+                self.scaleY = 1.0 ./ std(Y);
+                self.shiftU = mean(U);
+                self.shiftY = mean(Y);
+                fprintf('ESN scaling: standardize\n');
+
+            else
+                ME = MException('ESN:invalidParameter', ...
+                                'invalid scalingType parameter');
+                throw(ME);
+            end
+        end
+
+        %-------------------------------------------------------
         function [out] = scaleInput(self, in)
             out = (in - self.shiftU) .* self.scaleU;
         end
 
+        %-------------------------------------------------------
+        function [out] = scaleOutput(self, in)
+            out = (in - self.shiftY) .* self.scaleY;
+        end
+
+        %-------------------------------------------------------
+        function [out] = unscaleInput(self, in)
+            out = (in ./ self.scaleU ) + self.shiftU;
+        end
+
+        %-------------------------------------------------------
         function [out] = unscaleOutput(self, in)
             out = (in ./ self.scaleY ) + self.shiftY;
         end
